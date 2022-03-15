1d3bf221fc8b928fecb09ff8291bb4b07e3cbca0
tberkovac <tberkovac1@etf.unsa.ba>
2022-03-15 10:27:51 +0100

    Zadatak prvog tutorijala

---
+++ PrvaKonzolnaAplikacija/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace PrvaKonzolnaAplikacija
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite šest prirodnih brojeva (razdvojenih zarezom):");
+            string ulaz = Console.ReadLine();
+            string[] pojedinacniUlazi = ulaz.Split(",");
+            int[] brojevi = new int[6];
+            for (int i = 0; i < 6; i++)
+                brojevi[i] = Int32.Parse(pojedinacniUlazi[i]);    
+            
+            bool pozitivni = true;
+            int[] neparni = new int[6];
+            int brojNeparnih = 0;
+
+            foreach (var element in brojevi)
+            {
+                if (element <= 0)
+                    pozitivni = false;
+
+                if (element % 2 != 0)
+                    neparni[brojNeparnih++] = element;
+            }
+
+            string prikaz = pozitivni == true ? "nema" : "ima";
+            Console.WriteLine("Među unesenim brojevima " + prikaz + " negativnih brojeva.");
+            Console.WriteLine("Neparni brojevi: " + string.Join(" ", neparni[0..brojNeparnih]));
+        }
+
+         
+    }
+}
